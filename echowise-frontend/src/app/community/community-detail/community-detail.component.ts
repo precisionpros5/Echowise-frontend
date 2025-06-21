fb089084b21a807024a32e102175cfa04d264aeb
precisionpros5 <precision5pros@gmail.com>
2025-06-21 12:09:45 +0530

    user list display in community details

--- echowise-frontend/src/app/community/community-detail/community-detail.component.ts
+++ echowise-frontend/src/app/community/community-detail/community-detail.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-community-detail',
   standalone: true,
+  imports: [CommonModule, NgFor],
   templateUrl: './community-detail.component.html',
   styleUrls: ['./community-detail.component.css']
 })
@@ -10,8 +13,22 @@ export class CommunityDetailComponent {
   @Input() name!: string;
   @Input() code!: number;
   @Input() description!: string;
+  users: string[] = []; // Array to hold usernames of users in the community
 
   @Output() closePopup = new EventEmitter<void>(); // Emit event when popup is closed
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authService.getUsersInCommunity(this.code).subscribe({
+      next: (response: any[]) => {
+        this.users = response.map(user => user.username);
+      },
+      error: (err: any) => {
+        console.error('Failed to fetch users:', err);
+      }
+    });
+  }
 
   triggerClosePopup() {
     this.closePopup.emit(); // Notify parent component to close the popup
